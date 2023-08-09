d008ff77a31b441c90336b30072e6513aedb3483
Doggy-068 <728724654@qq.com>
2023-08-09 22:46:32 +0800

    feat: ws guard

--- app/router/ws.py
+++ app/router/ws.py
@@ -1,9 +1,18 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, Depends, Query, WebSocketException, status
+from jose import jwt
+from .auth import JWT_SECRET_KEY
 
 router = APIRouter(prefix='/api/ws')
 
 
-@router.websocket('')
+async def verify_token(token: str = Query()):
+    try:
+        jwt.decode(token, JWT_SECRET_KEY)
+    except:
+        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
+
+
+@router.websocket('', dependencies=[Depends(verify_token)])
 async def ws_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
